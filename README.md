083ecdc28e7d38ca7bbadb465eefb23bc65b42b2
David Vanderson <david.vanderson@gmail.com>
2023-04-12 15:41:22 -0400

    first examples

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# gui-demo
-examples for using gui
+# GUI Examples
+
+Here are some examples of how to use [gui](https://github.com/david-vanderson/gui). This repo serves as an example for how to integrate gui.
+
+- zig build run-standalone-sdl
+  - you want to build a new app
+  - gui will paint the whole window
+  - use this repo's build.zig and build.zig.zon as a starting point
+
+- zig build run-ontop-sdl
+  - you already have an app or game
+  - you want to add some gui stuff (like floating windows to for debugging)
+  - use this example for integration
+  - add [gui](https://github.com/david-vanderson/gui) as a git submodule
+
---
+++ build.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const gui_dep = b.dependency("gui", .{ .target = target, .optimize = optimize });
+
+    const examples = [_][]const u8{
+        "standalone-sdl",
+        "ontop-sdl",
+    };
+
+    inline for (examples) |ex| {
+        const exe = b.addExecutable(.{
+            .name = ex,
+            .root_source_file = .{ .path = ex ++ ".zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe.addModule("gui", gui_dep.module("gui"));
+        exe.addModule("SDLBackend", gui_dep.module("SDLBackend"));
+        const freetype_dep = gui_dep.builder.dependency("freetype", .{
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.linkLibrary(freetype_dep.artifact("freetype"));
+
+        exe.linkSystemLibrary("SDL2");
+        exe.linkLibC();
+
+        const compile_step = b.step(ex, "Compile " ++ ex);
+        compile_step.dependOn(&b.addInstallArtifact(exe).step);
+        b.getInstallStep().dependOn(compile_step);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(compile_step);
+
+        const run_step = b.step("run-" ++ ex, "Run " ++ ex);
+        run_step.dependOn(&run_cmd.step);
+    }
+}
