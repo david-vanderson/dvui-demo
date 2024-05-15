283a3b73a6567b67acbd34369708e9d12a521932
David Vanderson <david.vanderson@gmail.com>
2024-05-14 20:04:54 -0400

    update dvui and to zig 0.12

--- build.zig
+++ build.zig
@@ -19,10 +19,8 @@ pub fn build(b: *std.Build) !void {
             .optimize = optimize,
         });
 
-        exe.addModule("dvui", dvui_dep.module("dvui"));
-        exe.addModule("SDLBackend", dvui_dep.module("SDLBackend"));
-
-        link_deps(exe, dvui_dep);
+        exe.root_module.addImport("dvui", dvui_dep.module("dvui"));
+        exe.root_module.addImport("SDLBackend", dvui_dep.module("SDLBackend"));
 
         const compile_step = b.step(ex, "Compile " ++ ex);
         compile_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
@@ -33,78 +31,5 @@ pub fn build(b: *std.Build) !void {
 
         const run_step = b.step("run-" ++ ex, "Run " ++ ex);
         run_step.dependOn(&run_cmd.step);
-    }
-}
-
-fn link_deps(exe: *std.Build.Step.Compile, dvui_dep: *std.Build.Dependency) void {
-    const b = dvui_dep.builder;
-
-    //This should all be cleaned up when zig 0.12.x is released.
-    if (true) {
-        // Here we are trying to work within zig 0.11.x but not expose dvui's
-        // internal dependencies.
-        exe.linkLibrary(dvui_dep.artifact("dvui_libs"));
-        @import("root").dependencies.imports.dvui.add_include_paths(dvui_dep.builder, exe);
-    } else {
-        // Here we link the internal dependencies explicitly.
-
-        // TODO: remove this part about freetype (pulling it from the dvui_dep
-        // sub-builder) once https://github.com/ziglang/zig/pull/14731 lands
-
-        const freetype_dep = b.dependency("freetype", .{
-            .target = exe.target,
-            .optimize = exe.optimize,
-        });
-        exe.linkLibrary(freetype_dep.artifact("freetype"));
-
-        // TODO: remove this part about stb_image once either:
-        // - zig can successfully cimport stb_image.h
-        // - zig can have a module depend on a c file
-        const stbi_dep = b.dependency("stb_image", .{
-            .target = exe.target,
-            .optimize = exe.optimize,
-        });
-        exe.linkLibrary(stbi_dep.artifact("stb_image"));
-    }
-
-    exe.linkLibC();
-
-    if (exe.target.isWindows()) {
-        const sdl_dep = b.dependency("sdl", .{
-            .target = exe.target,
-            .optimize = exe.optimize,
-        });
-        exe.linkLibrary(sdl_dep.artifact("SDL2"));
-
-        exe.linkSystemLibrary("setupapi");
-        exe.linkSystemLibrary("winmm");
-        exe.linkSystemLibrary("gdi32");
-        exe.linkSystemLibrary("imm32");
-        exe.linkSystemLibrary("version");
-        exe.linkSystemLibrary("oleaut32");
-        exe.linkSystemLibrary("ole32");
-    } else {
-        if (exe.target.isDarwin()) {
-            exe.linkSystemLibrary("z");
-            exe.linkSystemLibrary("bz2");
-            exe.linkSystemLibrary("iconv");
-            exe.linkFramework("AppKit");
-            exe.linkFramework("AudioToolbox");
-            exe.linkFramework("Carbon");
-            exe.linkFramework("Cocoa");
-            exe.linkFramework("CoreAudio");
-            exe.linkFramework("CoreFoundation");
-            exe.linkFramework("CoreGraphics");
-            exe.linkFramework("CoreHaptics");
-            exe.linkFramework("CoreVideo");
-            exe.linkFramework("ForceFeedback");
-            exe.linkFramework("GameController");
-            exe.linkFramework("IOKit");
-            exe.linkFramework("Metal");
-        }
-
-        exe.linkSystemLibrary("SDL2");
-        //exe.addIncludePath(.{.path = "/Users/dvanderson/SDL2-2.24.1/include"});
-        //exe.addObjectFile(.{.path = "/Users/dvanderson/SDL2-2.24.1/build/.libs/libSDL2.a"});
     }
 }
