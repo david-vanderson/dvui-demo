d67546901591c8e5fa3e62cbc501147489e70add
David Vanderson <david.vanderson@gmail.com>
2025-03-19 11:16:32 -0400

    sync dvui: add web demo

--- build.zig
+++ build.zig
@@ -4,38 +4,77 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .sdl3 = true });
+    // SDL Examples
+    {
+        const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .backend = .sdl, .sdl3 = true });
 
-    const examples = [_][]const u8{
-        "sdl-standalone",
-        "sdl-ontop",
-    };
+        const examples = [_][]const u8{
+            "sdl-standalone",
+            "sdl-ontop",
+        };
 
-    inline for (examples) |ex| {
-        const exe = b.addExecutable(.{
-            .name = ex,
-            .root_source_file = b.path(ex ++ ".zig"),
-            .target = target,
-            .optimize = optimize,
+        inline for (examples) |ex| {
+            const exe = b.addExecutable(.{
+                .name = ex,
+                .root_source_file = b.path("sdl/" ++ ex ++ ".zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+
+            // Can either link the backend ourselves:
+            // const dvui_mod = dvui_dep.module("dvui");
+            // const sdl = dvui_dep.module("sdl");
+            // @import("dvui").linkBackend(dvui_mod, sdl);
+            // exe.root_module.addImport("dvui", dvui_mod);
+
+            // Or use a prelinked one:
+            exe.root_module.addImport("dvui", dvui_dep.module("dvui_sdl"));
+
+            const compile_step = b.step("compile-" ++ ex, "Compile " ++ ex);
+            compile_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
+            b.getInstallStep().dependOn(compile_step);
+
+            const run_cmd = b.addRunArtifact(exe);
+            run_cmd.step.dependOn(compile_step);
+
+            const run_step = b.step(ex, "Run " ++ ex);
+            run_step.dependOn(&run_cmd.step);
+        }
+    }
+
+    // Web Example
+    {
+        const web_target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
         });
 
-        // Can either link the backend ourselves:
-        // const dvui_mod = dvui_dep.module("dvui");
-        // const sdl = dvui_dep.module("sdl");
-        // @import("dvui").linkBackend(dvui_mod, sdl);
-        // exe.root_module.addImport("dvui", dvui_mod);
+        const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .backend = .web });
 
-        // Or use a prelinked one:
-        exe.root_module.addImport("dvui", dvui_dep.module("dvui_sdl"));
+        const web_test = b.addExecutable(.{
+            .name = "web-test",
+            .root_source_file = b.path("web/web-test.zig"),
+            .target = web_target,
+            .optimize = optimize,
+            .link_libc = false,
+            .strip = if (optimize == .ReleaseFast or optimize == .ReleaseSmall) true else false,
+        });
 
-        const compile_step = b.step("compile-" ++ ex, "Compile " ++ ex);
-        compile_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
+        web_test.entry = .disabled;
+        web_test.root_module.addImport("dvui", dvui_dep.module("dvui_web"));
+
+        const install_wasm = b.addInstallArtifact(web_test, .{
+            .dest_dir = .{ .override = .{ .custom = "bin" } },
+        });
+
+        const install_noto = b.addInstallBinFile(b.path("web/NotoSansKR-Regular.ttf"), "NotoSansKR-Regular.ttf");
+
+        const compile_step = b.step("web-test", "Compile the Web test");
+        compile_step.dependOn(&install_wasm.step);
+        compile_step.dependOn(&install_noto.step);
+        compile_step.dependOn(&b.addInstallFileWithDir(b.path("web/index.html"), .prefix, "bin/index.html").step);
+        const web_js = dvui_dep.namedLazyPath("web.js");
+        compile_step.dependOn(&b.addInstallFileWithDir(web_js, .prefix, "bin/web.js").step);
         b.getInstallStep().dependOn(compile_step);
-
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(compile_step);
-
-        const run_step = b.step(ex, "Run " ++ ex);
-        run_step.dependOn(&run_cmd.step);
     }
 }
---
+++ web/web-test.zig
@@ -0,0 +1,224 @@
+const std = @import("std");
+const dvui = @import("dvui");
+const WebBackend = dvui.backend;
+comptime {
+    std.debug.assert(@hasDecl(WebBackend, "WebBackend"));
+}
+usingnamespace WebBackend.wasm;
+
+const WriteError = error{};
+const LogWriter = std.io.Writer(void, WriteError, writeLog);
+
+fn writeLog(_: void, msg: []const u8) WriteError!usize {
+    WebBackend.wasm.wasm_log_write(msg.ptr, msg.len);
+    return msg.len;
+}
+
+pub fn logFn(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_txt = switch (message_level) {
+        .err => "error",
+        .warn => "warning",
+        .info => "info",
+        .debug => "debug",
+    };
+    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    const msg = level_txt ++ prefix2 ++ format ++ "\n";
+
+    (LogWriter{ .context = {} }).print(msg, args) catch return;
+    WebBackend.wasm.wasm_log_flush();
+}
+
+pub const std_options: std.Options = .{
+    // Overwrite default log handler
+    .logFn = logFn,
+};
+
+var gpa_instance = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = gpa_instance.allocator();
+
+var win: dvui.Window = undefined;
+var backend: WebBackend = undefined;
+var touchPoints: [2]?dvui.Point = [_]?dvui.Point{null} ** 2;
+var orig_content_scale: f32 = 1.0;
+
+const zig_favicon = @embedFile("src/zig-favicon.png");
+
+export fn app_init(platform_ptr: [*]const u8, platform_len: usize) i32 {
+    const platform = platform_ptr[0..platform_len];
+    dvui.log.debug("platform: {s}", .{platform});
+    const mac = if (std.mem.indexOf(u8, platform, "Mac") != null) true else false;
+
+    backend = WebBackend.init() catch {
+        return 1;
+    };
+    win = dvui.Window.init(@src(), gpa, backend.backend(), .{ .keybinds = if (mac) .mac else .windows }) catch {
+        return 2;
+    };
+
+    WebBackend.win = &win;
+
+    orig_content_scale = win.content_scale;
+
+    return 0;
+}
+
+export fn app_deinit() void {
+    win.deinit();
+    backend.deinit();
+}
+
+// return number of micros to wait (interrupted by events) for next frame
+// return -1 to quit
+export fn app_update() i32 {
+    return update() catch |err| {
+        std.log.err("{!}", .{err});
+        const msg = std.fmt.allocPrint(gpa, "{!}", .{err}) catch "allocPrint OOM";
+        WebBackend.wasm.wasm_panic(msg.ptr, msg.len);
+        return -1;
+    };
+}
+
+fn update() !i32 {
+    const nstime = win.beginWait(backend.hasEvent());
+
+    try win.begin(nstime);
+
+    // Instead of the backend saving the events and then calling this, the web
+    // backend is directly sending the events to dvui
+    //try backend.addAllEvents(&win);
+
+    try dvui_frame();
+    //try dvui.label(@src(), "test", .{}, .{ .color_text = .{ .color = dvui.Color.white } });
+
+    //var indices: []const u32 = &[_]u32{ 0, 1, 2, 0, 2, 3 };
+    //var vtx: []const dvui.Vertex = &[_]dvui.Vertex{
+    //    .{ .pos = .{ .x = 100, .y = 150 }, .uv = .{ 0.0, 0.0 }, .col = .{} },
+    //    .{ .pos = .{ .x = 200, .y = 150 }, .uv = .{ 1.0, 0.0 }, .col = .{ .g = 0, .b = 0, .a = 200 } },
+    //    .{ .pos = .{ .x = 200, .y = 250 }, .uv = .{ 1.0, 1.0 }, .col = .{ .r = 0, .b = 0, .a = 100 } },
+    //    .{ .pos = .{ .x = 100, .y = 250 }, .uv = .{ 0.0, 1.0 }, .col = .{ .r = 0, .g = 0 } },
+    //};
+    //backend.drawClippedTriangles(null, vtx, indices);
+
+    const end_micros = try win.end(.{});
+
+    backend.setCursor(win.cursorRequested());
+    backend.textInputRect(win.textInputRequested());
+
+    const wait_event_micros = win.waitTime(end_micros, null);
+    return @intCast(@divTrunc(wait_event_micros, 1000));
+}
+
+fn dvui_frame() !void {
+    var new_content_scale: ?f32 = null;
+    var old_dist: ?f32 = null;
+    for (dvui.events()) |*e| {
+        if (e.evt == .mouse and (e.evt.mouse.button == .touch0 or e.evt.mouse.button == .touch1)) {
+            const idx: usize = if (e.evt.mouse.button == .touch0) 0 else 1;
+            switch (e.evt.mouse.action) {
+                .press => {
+                    touchPoints[idx] = e.evt.mouse.p;
+                },
+                .release => {
+                    touchPoints[idx] = null;
+                },
+                .motion => {
+                    if (touchPoints[0] != null and touchPoints[1] != null) {
+                        e.handled = true;
+                        var dx: f32 = undefined;
+                        var dy: f32 = undefined;
+
+                        if (old_dist == null) {
+                            dx = touchPoints[0].?.x - touchPoints[1].?.x;
+                            dy = touchPoints[0].?.y - touchPoints[1].?.y;
+                            old_dist = @sqrt(dx * dx + dy * dy);
+                        }
+
+                        touchPoints[idx] = e.evt.mouse.p;
+
+                        dx = touchPoints[0].?.x - touchPoints[1].?.x;
+                        dy = touchPoints[0].?.y - touchPoints[1].?.y;
+                        const new_dist: f32 = @sqrt(dx * dx + dy * dy);
+
+                        new_content_scale = @max(0.1, win.content_scale * new_dist / old_dist.?);
+                    }
+                },
+                else => {},
+            }
+        }
+    }
+
+    {
+        var m = try dvui.menu(@src(), .horizontal, .{ .background = true, .expand = .horizontal });
+        defer m.deinit();
+
+        if (try dvui.menuItemLabel(@src(), "File", .{ .submenu = true }, .{ .expand = .none })) |r| {
+            var fw = try dvui.floatingMenu(@src(), dvui.Rect.fromPoint(dvui.Point{ .x = r.x, .y = r.y + r.h }), .{});
+            defer fw.deinit();
+
+            if (try dvui.menuItemLabel(@src(), "Close Menu", .{}, .{}) != null) {
+                m.close();
+            }
+        }
+
+        if (try dvui.menuItemLabel(@src(), "Edit", .{ .submenu = true }, .{ .expand = .none })) |r| {
+            var fw = try dvui.floatingMenu(@src(), dvui.Rect.fromPoint(dvui.Point{ .x = r.x, .y = r.y + r.h }), .{});
+            defer fw.deinit();
+            _ = try dvui.menuItemLabel(@src(), "Dummy", .{}, .{ .expand = .horizontal });
+            _ = try dvui.menuItemLabel(@src(), "Dummy Long", .{}, .{ .expand = .horizontal });
+            _ = try dvui.menuItemLabel(@src(), "Dummy Super Long", .{}, .{ .expand = .horizontal });
+        }
+    }
+
+    var scroll = try dvui.scrollArea(@src(), .{}, .{ .expand = .both, .color_fill = .{ .name = .fill_window } });
+    defer scroll.deinit();
+
+    var tl = try dvui.textLayout(@src(), .{}, .{ .expand = .horizontal, .font_style = .title_4 });
+    const lorem = "This example shows how to use dvui in a web canvas.";
+    try tl.addText(lorem, .{});
+    tl.deinit();
+
+    var tl2 = try dvui.textLayout(@src(), .{}, .{ .expand = .horizontal });
+    try tl2.format(
+        \\DVUI
+        \\- paints the entire window
+        \\- can show floating windows and dialogs
+        \\- example menu at the top of the window
+        \\- rest of the window is a scroll area
+        \\
+        \\backend: {s}
+    , .{backend.about()}, .{});
+    try tl2.addText("\n\n", .{});
+    try tl2.addText("Framerate is variable and adjusts as needed for input events and animations.", .{});
+    try tl2.addText("\n\n", .{});
+    try tl2.addText("Cursor is always being set by dvui.", .{});
+    try tl2.addText("\n\n", .{});
+    if (dvui.useFreeType) {
+        try tl2.addText("Fonts are being rendered by FreeType 2.", .{});
+    } else {
+        try tl2.addText("Fonts are being rendered by stb_truetype.", .{});
+    }
+    try tl2.addText("\n\n", .{});
+    try tl2.format("Scale: {d:0.2} (try pinch-zoom)", .{dvui.windowNaturalScale()}, .{});
+    tl2.deinit();
+
+    if (try dvui.button(@src(), "Reset Scale", .{}, .{})) {
+        new_content_scale = orig_content_scale;
+    }
+
+    const label = if (dvui.Examples.show_demo_window) "Hide Demo Window" else "Show Demo Window";
+    if (try dvui.button(@src(), label, .{}, .{})) {
+        dvui.Examples.show_demo_window = !dvui.Examples.show_demo_window;
+    }
+
+    // look at demo() for examples of dvui widgets, shows in a floating window
+    try dvui.Examples.demo();
+
+    if (new_content_scale) |ns| {
+        win.content_scale = ns;
+    }
+}
